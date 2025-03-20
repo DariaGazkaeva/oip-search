01da3bba1bd69bd330603dd6b8290b1ca1347c01
Dasha <d.gazkaeva@gmail.com>
2025-03-20 20:26:31 +0300

    two files for each page

--- task_2/create_tokens.py
+++ task_2/create_tokens.py
@@ -24,16 +24,19 @@ def is_russian(word: str) -> bool:
     return bool(re.match(r"^[а-яё]+$", word, re.IGNORECASE))
 
 
-def process_pages(
-    directory: str, output_tokens_file: str, output_lemma_file: str
-) -> None:
-    tokens = set()
-    lemma_to_tokens = {}
+def process_pages(directory: str) -> None:
 
     stop_words = set(stopwords.words("russian"))
     morph = pymorphy2.MorphAnalyzer()
 
     for filename in os.listdir(directory):
+        file_number = filename[:-5]
+        output_tokens_file = f"results/tokens_{file_number}.txt"
+        output_lemma_file = f"results/lemmas_{file_number}.txt"
+
+        tokens = set()
+        lemma_to_tokens = {}
+
         filepath = os.path.join(directory, filename)
         try:
             with open(filepath, "r", encoding="utf-8") as f:
@@ -45,7 +48,7 @@ def process_pages(
             )  # токенизация и приведение к нижнему регистру
 
             for word in words:
-                # фильтрация (только из букв, не стоп-слова, без цифр, длиной больше 1)
+                # фильтрация (только из русских букв, не стоп-слова, без цифр, длиной больше 1)
                 if (
                     is_russian(word)
                     and word not in stop_words
@@ -54,32 +57,32 @@ def process_pages(
                 ):
                     tokens.add(word)
 
+            # лемматизация
+            for token in tokens:
+                parsed_word = morph.parse(token)[0]
+                lemma = parsed_word.normal_form
+                if lemma not in lemma_to_tokens:
+                    lemma_to_tokens[lemma] = set()
+                lemma_to_tokens[lemma].add(token)
+
+            # сохранение в файлы
+            try:
+                with open(output_tokens_file, "w+", encoding="utf-8") as f:
+                    for token in tokens:
+                        f.write(token + "\n")
+
+                with open(output_lemma_file, "w+", encoding="utf-8") as f:
+                    for lemma, token_set in lemma_to_tokens.items():
+                        f.write(lemma + ": " + " ".join(token_set) + "\n")
+
+                print(f"Токены сохранены в {output_tokens_file}")
+                print(f"Лемматизированные токены сохранены в {output_lemma_file}")
+
+            except Exception as e:
+                print(f"Ошибка при сохранении файлов: {e}")
+
         except Exception as e:
             print(f"Ошибка при обработке файла {filename}: {e}")
-
-    # лемматизация
-    for token in tokens:
-        parsed_word = morph.parse(token)[0]
-        lemma = parsed_word.normal_form
-        if lemma not in lemma_to_tokens:
-            lemma_to_tokens[lemma] = set()
-        lemma_to_tokens[lemma].add(token)
-
-    # сохранение в файлы
-    try:
-        with open(output_tokens_file, "w+", encoding="utf-8") as f:
-            for token in tokens:
-                f.write(token + "\n")
-
-        with open(output_lemma_file, "w+", encoding="utf-8") as f:
-            for lemma, token_set in lemma_to_tokens.items():
-                f.write(lemma + ": " + " ".join(token_set) + "\n")
-
-        print(f"Токены сохранены в {output_tokens_file}")
-        print(f"Лемматизированные токены сохранены в {output_lemma_file}")
-
-    except Exception as e:
-        print(f"Ошибка при сохранении файлов: {e}")
 
 
 if __name__ == "__main__":
@@ -91,7 +94,5 @@ if __name__ == "__main__":
         exit(1)
     print(f"Ищем скачанные страницы в директории {directory_path}")
     os.makedirs("results", exist_ok=True)
-    output_tokens_file = "results/tokens.txt"
-    output_lemma_file = "results/lemma_tokens.txt"
 
-    process_pages(directory_path, output_tokens_file, output_lemma_file)
+    process_pages(directory_path)
